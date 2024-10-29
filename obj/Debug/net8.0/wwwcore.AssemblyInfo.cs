aa85426d6740aad033b493dc66efc3a05cd8bb35
rtink-git <129110347+rtink-git@users.noreply.github.com>
2024-10-30 04:48:47 +0500

    Release 1.0.6
    A link to the github repository has been added

--- obj/Debug/net8.0/wwwcore.AssemblyInfo.cs
+++ obj/Debug/net8.0/wwwcore.AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("wwwcore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.5.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.5+617b861bea573552426a5e48ce5c2978479a350c")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.6.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.6+3fc9a6a67ac66608f69c731a2661e3e7a5b8a795")]
 [assembly: System.Reflection.AssemblyProductAttribute("wwwcore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wwwcore")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.5.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.6.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
